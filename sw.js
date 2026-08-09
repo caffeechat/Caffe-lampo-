369767e69ad6b765ca25bc8c20724dd6e9e377f7
caffeechat <spanoannamaria007@gmail.com>
2026-08-09 22:47:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,11 @@
-const CACHE_NAME = 'pausa-caffe-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './icona.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Gestione rete
 });
