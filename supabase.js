231ecf91931278cf9ea58c5297d4aba06ff22681
caffeechat <spanoannamaria007@gmail.com>
2026-08-09 22:45:59 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://duddogkjpkojlvhazswf.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_UB-MxEcMZrA55ZZEOLzTIA_toqD1x-r';
+
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
